295afda1894eb5338377b1a6673cb8c631dbcce3
Rahul <mtal3584@gmail.com>
2023-12-31 22:29:16 +0545

    cart provider and screens added

--- ecommerce_app/lib/screens/products_overview_page.dart
+++ ecommerce_app/lib/screens/products_overview_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
+import './cart_screen.dart';
+import '../providers/products.dart';
 
 enum FilterOptions {
   Favorites,
@@ -13,8 +21,34 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+  var _isLoading = false;
+  final _isInit = true;
+
+  // @override
+  // void didChangeDependencies() {
+  //   if (_isInit) {
+  //     _isLoading = true;
+  //     Provider.of<Products>(context, listen: false)
+  //         .fetchAndSetProducts()
+  //         .then((_) => _isLoading = false);
+  //   }
+  //   super.didChangeDependencies();
+  // }
+
   @override
-  void initState() {}
+  void initState() {
+    setState(() => _isLoading = true);
+
+    Provider.of<Products>(context, listen: false)
+        .fetchAndSetProducts()
+        .then((_) {
+      print("products fetched and set");
+      setState(() => _isLoading = false);
+    });
+    // print("products fetched and set");
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +58,58 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             IconThemeData(color: Theme.of(context).colorScheme.secondary),
         backgroundColor: Theme.of(context).primaryColor,
         title: Text(
-          'E-Com',
+          'Hamro Shop',
           style: TextStyle(color: Theme.of(context).colorScheme.secondary),
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(
+              Icons.more_horiz,
+              color: Theme.of(context).colorScheme.secondary,
+              size: 30,
+            ),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorites,
+                child: Text('Only Favorites'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Show All'),
+              ),
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => badge(
+              value: cart.itemCount.toString(),
+              child: ch as Widget,
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).colorScheme.secondary,
+                size: 30,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          ),
+        ],
       ),
+      drawer: const AppDrawer(),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ProductsGrid(_showOnlyFavorites),
     );
   }
 }
---
+++ ecommerce_app/lib/widgets/user_product_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const UserProductItem(this.id, this.title, this.imageUrl, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    // TODO: implement build
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(backgroundImage: NetworkImage(imageUrl)),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(children: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
+              icon: const Icon(Icons.edit)),
+          IconButton(
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("Deleting Failed"),
+                  ));
+                }
+              },
+              icon: Icon(
+                Icons.delete,
+                color: Theme.of(context).colorScheme.error,
+              )),
+        ]),
+      ),
+    );
+  }
+}
